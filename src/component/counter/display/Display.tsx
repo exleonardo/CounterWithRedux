3936a827c9a7d925409f5001e589dcdd595a057e
exleonardo <exleonardo@mail.ru>
2023-08-26 18:44:34 +0300

    rename style and add style for Display.module.css

--- src/component/counter/display/Display.tsx
+++ src/component/counter/display/Display.tsx
@@ -1,11 +1,11 @@
-import React , {useEffect} from 'react';
+import React from 'react';
 import Grid from "@mui/material/Unstable_Grid2";
 import {Alert} from "@mui/material";
 import Button from "@mui/material/Button";
 import {useDispatch , useSelector} from "react-redux";
 import {AppRootStateType , DisplayType , SettingType} from "../../store/store";
 import {incrementValueAC , resetValueAC , switchIncrementAC} from "../../reducers/displayReducer";
-import s from "./Setting.module.css"
+import s from "./Display.module.css"
 
 const Display = () => {
     const display = useSelector<AppRootStateType , DisplayType> ( (state) => state.display[0] )
@@ -20,11 +20,12 @@ const Display = () => {
         dispatch ( resetValueAC ( setting.inputMinValue ) )
         dispatch ( switchIncrementAC () )
     }
+    const divClassName = s.errorMessage + (display.buttonIncrementError ? ' ' + s.displayErrorRed : '')
     return (
         <Grid xs={6} className={s.display}>
             {display.errorMessage ?
                 <Alert severity="error" className={s.errorMessage}>{display.errorMessage}</Alert> :
-                <div className={s.errorMessage}>{display.minValue}</div>}
+                <div className={divClassName}>{display.minValue}</div>}
             <div className={s.button}>
                 <Button variant="contained" size='small'
                         disabled={display.buttonIncrementError}
